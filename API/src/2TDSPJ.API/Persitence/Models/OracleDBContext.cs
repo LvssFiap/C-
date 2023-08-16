d97044f5d668323ec890de5aa344c35583988615
LvssFiap <142401645+LvssFiap@users.noreply.github.com>
2023-08-16 09:47:47 -0300

    Add files via upload

---
+++ API/src/2TDSPJ.API/Persitence/Models/OracleDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _2TDSPJ.API.Persitence.Models
+{
+    public class OracleDBContext: DbContext
+    {
+
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Turma> Turmas { get; set; }
+        public OracleDBContext(DbContextOptions<OracleDBContext> options) : base(options) 
+        {
+            
+        }
+
+
+    }
+}
---
+++ API/src/2TDSPJ.API/Persitence/Models/Turma.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace _2TDSPJ.API.Persitence.Models
+{
+    public class Turma
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TurmaId { get; set; }
+
+        [Required(ErrorMessage = "Campo não pode ser nulo")]
+        public string Sigla { get; set; }
+
+       
+        public string Sala { get; set; }
+
+        [Required]
+        public bool Ativo { get; set; }
+    }
+}
